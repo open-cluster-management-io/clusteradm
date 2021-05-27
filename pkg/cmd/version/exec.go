43419a1500eeb1a3185fc2268d65fbb4b8a52d5a
Dominique Vernier <dvernier@redhat.com>
2021-05-26 21:55:40 -0400

    remove acm portion of exec
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- pkg/cmd/version/exec.go
+++ pkg/cmd/version/exec.go
@@ -2,13 +2,7 @@
 package version
 
 import (
-	"context"
 	"fmt"
-	"strings"
-
-	apps "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
 
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -34,46 +28,14 @@ func (o *Options) run() (err error) {
 }
 
 func (o *Options) runWithClient(client crclient.Client) (err error) {
-	cms := &corev1.ConfigMapList{}
-	ls := labels.SelectorFromSet(labels.Set{
-		"ocm-configmap-type": "image-manifest",
-	})
-	err = client.List(context.TODO(), cms, &crclient.ListOptions{LabelSelector: ls})
+	discoveryClient, err := o.factory.ToDiscoveryClient()
 	if err != nil {
 		return err
 	}
-	if len(cms.Items) > 1 {
-		return fmt.Errorf("found more than one configmap with labelset %v", ls)
+	serverVersion, err := discoveryClient.ServerVersion()
+	if err != nil {
+		return err
 	}
-	//ACM environment
-	if len(cms.Items) != 0 {
-		if v, ok := cms.Items[0].Labels["ocm-release-version"]; ok {
-			fmt.Printf("server release\tversion\t:%s\n", v)
-		} else {
-			fmt.Printf("server release\tversion\t: not found")
-		}
-		ns := cms.Items[0].Namespace
-		acmRegistryDeployment := &apps.Deployment{}
-		err = client.Get(context.TODO(), crclient.ObjectKey{Name: "acm-custom-registry", Namespace: ns}, acmRegistryDeployment)
-		if err != nil {
-			return nil
-		}
-		for _, c := range acmRegistryDeployment.Spec.Template.Spec.Containers {
-			if strings.Contains(c.Image, "acm-custom-registry") {
-				fmt.Printf("server image\ttag\t:%s\n", strings.Split(c.Image, ":")[1])
-				break
-			}
-		}
-	} else {
-		discoveryClient, err := o.factory.ToDiscoveryClient()
-		if err != nil {
-			return err
-		}
-		serverVersion, err := discoveryClient.ServerVersion()
-		if err != nil {
-			return err
-		}
-		fmt.Printf("server release\tversion\t:%s\n", serverVersion.GitVersion)
-	}
+	fmt.Printf("server release\tversion\t:%s\n", serverVersion.GitVersion)
 	return nil
 }

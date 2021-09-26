040d28a288ca7a8891b5aeba44a1eaaf7fed46f7
Leilei Hu <leileihu@cn.ibm.com>
2021-09-26 11:44:58 +0800

    add the unjoin feature
    Signed-off-by: Leilei Hu <leileihu@cn.ibm.com>

---
+++ pkg/cmd/unjoin/cmd.go
@@ -0,0 +1,49 @@
+// Copyright Contributors to the Open Cluster Management project
+package unjoin
+
+import (
+	"fmt"
+
+	genericclioptionsclusteradm "open-cluster-management.io/clusteradm/pkg/genericclioptions"
+	"open-cluster-management.io/clusteradm/pkg/helpers"
+
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+var example = `
+# UnJoin a cluster to the hub
+%[1]s unjoin --cluster-name <cluster_name>
+`
+
+// NewCmd ...
+func NewCmd(clusteradmFlags *genericclioptionsclusteradm.ClusteradmFlags, streams genericclioptions.IOStreams) *cobra.Command {
+	o := newOptions(clusteradmFlags, streams)
+
+	cmd := &cobra.Command{
+		Use:          "unjoin",
+		Short:        "unjoin from a hub",
+		Example:      fmt.Sprintf(example, helpers.GetExampleHeader()),
+		SilenceUsage: true,
+		PreRun: func(c *cobra.Command, args []string) {
+			helpers.DryRunMessage(o.ClusteradmFlags.DryRun)
+		},
+		RunE: func(c *cobra.Command, args []string) error {
+			if err := o.complete(c, args); err != nil {
+				return err
+			}
+			if err := o.validate(); err != nil {
+				return err
+			}
+			if err := o.run(); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&o.clusterName, "cluster-name", "", "The name of the joining cluster")
+	cmd.Flags().BoolVar(&o.purgeOperator, "purge-operator", true, "Purge the operator")
+	cmd.Flags().StringVar(&o.outputFile, "output-file", "", "The generated resources will be copied in the specified file")
+	return cmd
+}
---
+++ pkg/cmd/unjoin/options.go
@@ -0,0 +1,30 @@
+// Copyright Contributors to the Open Cluster Management project
+package unjoin
+
+import (
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	genericclioptionsclusteradm "open-cluster-management.io/clusteradm/pkg/genericclioptions"
+)
+
+//Options: The structure holding all the command-line options
+type Options struct {
+	//ClusteradmFlags: The generic optiosn from the clusteradm cli-runtime.
+	ClusteradmFlags *genericclioptionsclusteradm.ClusteradmFlags
+	//The name under the cluster must be imported
+	clusterName string
+	//Delete the operator by default
+	purgeOperator bool
+	//The file to output the resources will be sent to the file.
+	outputFile string
+	values     Values
+}
+type Values struct {
+	//ClusterName: the name of the joined cluster on the hub
+	ClusterName string
+}
+
+func newOptions(clusteradmFlags *genericclioptionsclusteradm.ClusteradmFlags, streams genericclioptions.IOStreams) *Options {
+	return &Options{
+		ClusteradmFlags: clusteradmFlags,
+	}
+}

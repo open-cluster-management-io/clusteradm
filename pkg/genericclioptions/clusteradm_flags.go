9f30d22843a229fffcd41c5c04b744f444183f4f
Ilan Pinto <ilpinto@redhat.com>
2021-11-25 22:36:16 +0200

    This is a combination of 2 commits.
    fixed #68 - added timeout flag
    
    short flag name
    
    fixed PR comments
    
    added tested for timeout
    
    removed timout from Options.go
    
    Signed-off-by: Ilan Pinto <ilpinto@redhat.com>

--- pkg/genericclioptions/clusteradm_flags.go
+++ pkg/genericclioptions/clusteradm_flags.go
@@ -9,7 +9,8 @@ import (
 type ClusteradmFlags struct {
 	KubectlFactory cmdutil.Factory
 	//if set the resources will be sent to stdout instead of being applied
-	DryRun bool
+	DryRun bool;
+	Timeout int
 }
 
 // NewClusteradmFlags returns ClusteradmFlags with default values set
@@ -21,4 +22,5 @@ func NewClusteradmFlags(f cmdutil.Factory) *ClusteradmFlags {
 
 func (f *ClusteradmFlags) AddFlags(flags *pflag.FlagSet) {
 	flags.BoolVar(&f.DryRun, "dry-run", false, "If set the generated resources will be displayed but not applied")
+	flags.IntVar(&f.Timeout, "timeout", 300, "extend timeout from 300 secounds ")
 }

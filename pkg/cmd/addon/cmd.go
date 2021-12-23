6a2800d4fae7bff3ce2b37df9228462db12dd87b
ycyaoxdu <yaoyuchen0626@163.com>
2021-12-23 17:39:35 +0800

    addon enable: allow certain addon to be deployed
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

--- pkg/cmd/addon/cmd.go
+++ pkg/cmd/addon/cmd.go
@@ -5,7 +5,6 @@ import (
 	"github.com/spf13/cobra"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"open-cluster-management.io/clusteradm/pkg/cmd/addon/enable"
-
 	genericclioptionsclusteradm "open-cluster-management.io/clusteradm/pkg/genericclioptions"
 )
 

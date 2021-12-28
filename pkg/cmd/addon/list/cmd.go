067ce790e2fc8df7b4b2414c5039b623c80984a0
ycyaoxdu <yaoyuchen0626@163.com>
2021-12-28 14:35:48 +0800

    addon list: list enabled addon on specified managedcluster
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

---
+++ pkg/cmd/addon/list/cmd.go
@@ -0,0 +1,51 @@
+// Copyright Contributors to the Open Cluster Management project
+package list
+
+import (
+	"fmt"
+
+	genericclioptionsclusteradm "open-cluster-management.io/clusteradm/pkg/genericclioptions"
+	clusteradmhelpers "open-cluster-management.io/clusteradm/pkg/helpers"
+
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+)
+
+var example = `
+# List enabled addon on a specified cluster
+%[1]s addon list --cluster cluster1
+`
+
+// NewCmd...
+func NewCmd(clusteradmFlags *genericclioptionsclusteradm.ClusteradmFlags, streams genericclioptions.IOStreams) *cobra.Command {
+	o := newOptions(clusteradmFlags, streams)
+
+	cmd := &cobra.Command{
+		Use:          "list",
+		Short:        "list enabled addon on specified managedcluster",
+		Example:      fmt.Sprintf(example, clusteradmhelpers.GetExampleHeader()),
+		SilenceUsage: true,
+		PreRunE: func(c *cobra.Command, args []string) error {
+			clusteradmhelpers.DryRunMessage(clusteradmFlags.DryRun)
+
+			return nil
+		},
+		RunE: func(c *cobra.Command, args []string) error {
+			if err := o.complete(c, args); err != nil {
+				return err
+			}
+			if err := o.validate(); err != nil {
+				return err
+			}
+			if err := o.run(); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&o.clusters, "cluster", "", "Names of the managed cluster to deploy the add-on to (comma separated)")
+
+	return cmd
+}

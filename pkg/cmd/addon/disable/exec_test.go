ec7df62b1f24e633797b498b170ac48d45b65048
ycyaoxdu <yaoyuchen0626@163.com>
2022-01-17 11:56:44 +0800

    add integration test for clusteradm addon disable
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

---
+++ pkg/cmd/addon/disable/exec_test.go
@@ -0,0 +1,129 @@
+// Copyright Contributors to the Open Cluster Management project
+package disable
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+
+	clusterapiv1 "open-cluster-management.io/api/cluster/v1"
+
+	"open-cluster-management.io/clusteradm/pkg/cmd/addon/enable"
+	"open-cluster-management.io/clusteradm/pkg/cmd/addon/enable/scenario"
+
+	"open-cluster-management.io/clusteradm/pkg/helpers/apply"
+)
+
+var _ = ginkgo.Describe("addon disable", func() {
+	var cluster1Name string
+	var cluster2Name string
+	var err error
+
+	appMgrAddonName := "application-manager"
+
+	ginkgo.BeforeEach(func() {
+		cluster1Name = fmt.Sprintf("cluster-%s", rand.String(5))
+		cluster2Name = fmt.Sprintf("cluster-%s", rand.String(5))
+	})
+
+	assertCreatingClusters := func(clusterName string) {
+		ginkgo.By(fmt.Sprintf("Create %s cluster", clusterName))
+
+		cluster := &clusterapiv1.ManagedCluster{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: clusterName,
+			},
+		}
+
+		_, err = clusterClient.ClusterV1().ManagedClusters().Create(context.Background(), cluster, metav1.CreateOptions{})
+		gomega.Expect(err).ToNot(gomega.HaveOccurred())
+
+		ns := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: clusterName,
+			},
+		}
+		_, err := kubeClient.CoreV1().Namespaces().Create(context.Background(), ns, metav1.CreateOptions{})
+		gomega.Expect(err).ToNot(gomega.HaveOccurred(), "creat cluster error")
+	}
+
+	streams := genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr}
+
+	assertEnableAddon := func(addons []string, clusters []string, ns string) {
+
+		reader := scenario.GetScenarioResourcesReader()
+		applierBuilder := &apply.ApplierBuilder{}
+		applier := applierBuilder.WithClient(kubeClient, apiExtensionsClient, dynamicClient).Build()
+
+		for _, addon := range addons {
+			for _, clus := range clusters {
+				ginkgo.By(fmt.Sprintf("Enableing %s addon on %s cluster in %s namespce", addon, clus, ns))
+
+				cai := enable.NewClusterAddonInfo(clus, ns, addon)
+				_, err := applier.ApplyCustomResources(reader, cai, false, "", "addons/app/addon.yaml")
+				gomega.Expect(err).ToNot(gomega.HaveOccurred(), "enable addon error")
+				fmt.Fprintf(streams.Out, "Deploying %s add-on to namespaces %s of managed cluster: %s.\n", addon, ns, clus)
+			}
+		}
+	}
+
+	ginkgo.Context("runWithClient", func() {
+
+		ginkgo.It("Should disable application-manager ManagedClusterAddOn in ManagedCluster namespace successfully", func() {
+			assertCreatingClusters(cluster1Name)
+
+			addons := []string{appMgrAddonName}
+			clusters := []string{cluster1Name}
+			assertEnableAddon([]string{appMgrAddonName}, []string{cluster1Name}, "default")
+
+			o := Options{
+				namespace: "open-cluster-management-agent-addon",
+				Streams:   streams,
+			}
+
+			err := o.runWithClient(clusterClient, addonClient, kubeClient, apiExtensionsClient, dynamicClient, false, addons, clusters)
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		})
+
+		ginkgo.It("Should disable application-manager ManagedClusterAddOns in each ManagedCluster namespace successfully", func() {
+			assertCreatingClusters(cluster1Name)
+			assertCreatingClusters(cluster2Name)
+
+			addons := []string{appMgrAddonName}
+			clusters := []string{cluster1Name, cluster2Name}
+			assertEnableAddon(addons, clusters, "default")
+
+			o := Options{
+				namespace: "open-cluster-management-agent-addon",
+				Streams:   streams,
+			}
+
+			err := o.runWithClient(clusterClient, addonClient, kubeClient, apiExtensionsClient, dynamicClient, false, addons, clusters)
+			gomega.Expect(err).ToNot(gomega.HaveOccurred())
+		})
+
+		ginkgo.It("Should not disable a ManagedClusterAddOn because ManagedCluster doesn't exist", func() {
+			assertCreatingClusters(cluster1Name)
+
+			addons := []string{appMgrAddonName}
+			clusters := []string{cluster1Name}
+			assertEnableAddon(addons, clusters, "default")
+
+			wrongCluster := "no-such-addon"
+			wrongClusters := []string{wrongCluster}
+			o := Options{
+				Streams: streams,
+			}
+
+			err := o.runWithClient(clusterClient, addonClient, kubeClient, apiExtensionsClient, dynamicClient, false, addons, wrongClusters)
+			gomega.Expect(err).To(gomega.HaveOccurred())
+		})
+	})
+})
---
+++ pkg/cmd/addon/disable/suite_test.go
@@ -0,0 +1,70 @@
+// Copyright Contributors to the Open Cluster Management project
+package disable
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega"
+
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	addonv1alpha1client "open-cluster-management.io/api/client/addon/clientset/versioned"
+	clusterv1client "open-cluster-management.io/api/client/cluster/clientset/versioned"
+)
+
+var testEnv *envtest.Environment
+var restConfig *rest.Config
+var kubeClient kubernetes.Interface
+var apiExtensionsClient apiextensionsclient.Interface
+var dynamicClient dynamic.Interface
+var clusterClient clusterv1client.Interface
+var addonClient addonv1alpha1client.Interface
+
+func TestIntegrationEnableAddons(t *testing.T) {
+	gomega.RegisterFailHandler(ginkgo.Fail)
+	ginkgo.RunSpecs(t, "Integration Disable Addons Suite")
+}
+
+var _ = ginkgo.BeforeSuite(func(done ginkgo.Done) {
+	ginkgo.By("bootstrapping test environment")
+
+	// start a kube-apiserver
+	testEnv = &envtest.Environment{
+		ErrorIfCRDPathMissing: true,
+		CRDDirectoryPaths: []string{
+			filepath.Join("..", "..", "..", "..", "vendor", "open-cluster-management.io", "api", "cluster", "v1"),
+			filepath.Join("..", "..", "..", "..", "vendor", "open-cluster-management.io", "api", "addon", "v1alpha1"),
+		},
+	}
+
+	cfg, err := testEnv.Start()
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+	gomega.Expect(cfg).ToNot(gomega.BeNil())
+
+	kubeClient, err = kubernetes.NewForConfig(cfg)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	apiExtensionsClient, err = apiextensionsclient.NewForConfig(cfg)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	dynamicClient, err = dynamic.NewForConfig(cfg)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	clusterClient, err = clusterv1client.NewForConfig(cfg)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+	addonClient, err = addonv1alpha1client.NewForConfig(cfg)
+	gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+	restConfig = cfg
+	close(done)
+}, 60)
+
+var _ = ginkgo.AfterSuite(func() {
+	ginkgo.By("tearing down the test environment")
+
+	err := testEnv.Stop()
+	gomega.Expect(err).ToNot(gomega.HaveOccurred())
+})

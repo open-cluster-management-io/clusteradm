7834f44cdade632fe1548c610a634f78827262bb
yue9944882 <291271447@qq.com>
2022-02-15 18:03:37 +0800

    feat: clusteradm get hub-info
    Signed-off-by: yue9944882 <291271447@qq.com>

--- pkg/helpers/print.go
+++ pkg/helpers/print.go
@@ -2,6 +2,8 @@
 package helpers
 
 import (
+	"fmt"
+	"io"
 	"time"
 
 	"github.com/briandowns/spinner"
@@ -10,6 +12,60 @@ import (
 )
 
 var suffixColor = color.New(color.Bold, color.FgGreen)
+
+// PrefixWriter can write text at various indentation levels.
+type PrefixWriter interface {
+	// Write writes text with the specified indentation level.
+	Write(level int, format string, a ...interface{})
+	// WriteLine writes an entire line with no indentation level.
+	WriteLine(a ...interface{})
+	// Flush forces indentation to be reset.
+	Flush()
+}
+
+// Each level has 2 spaces for PrefixWriter
+const (
+	LEVEL_0 = iota
+	LEVEL_1
+	LEVEL_2
+	LEVEL_3
+	LEVEL_4
+)
+
+// prefixWriter implements PrefixWriter
+type prefixWriter struct {
+	out io.Writer
+}
+
+var _ PrefixWriter = &prefixWriter{}
+
+// NewPrefixWriter creates a new PrefixWriter.
+func NewPrefixWriter(out io.Writer) PrefixWriter {
+	return &prefixWriter{out: out}
+}
+
+func (pw *prefixWriter) Write(level int, format string, a ...interface{}) {
+	levelSpace := "  "
+	prefix := ""
+	for i := 0; i < level; i++ {
+		prefix += levelSpace
+	}
+	fmt.Fprintf(pw.out, prefix+format, a...)
+}
+
+func (pw *prefixWriter) WriteLine(a ...interface{}) {
+	fmt.Fprintln(pw.out, a...)
+}
+
+func (pw *prefixWriter) Flush() {
+	if f, ok := pw.out.(flusher); ok {
+		f.Flush()
+	}
+}
+
+type flusher interface {
+	Flush()
+}
 
 func NewSpinner(suffix string, interval time.Duration) *spinner.Spinner {
 	return spinner.New(

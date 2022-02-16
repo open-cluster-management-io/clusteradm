50b515f763ef11d47e03c7478ad1e8041ddcafc7
yue9944882 <291271447@qq.com>
2022-02-16 16:23:04 +0800

    get addon: tree view
    Signed-off-by: yue9944882 <291271447@qq.com>

--- pkg/cmd/get/addon/exec.go
+++ pkg/cmd/get/addon/exec.go
@@ -3,22 +3,21 @@ package addon
 
 import (
 	"context"
-	"sort"
+	"fmt"
 
+	"github.com/disiqueira/gotree"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
-	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/sets"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/klog/v2"
 	"open-cluster-management.io/api/addon/v1alpha1"
 	addonclient "open-cluster-management.io/api/client/addon/clientset/versioned"
 	clusterclientset "open-cluster-management.io/api/client/cluster/clientset/versioned"
-	"open-cluster-management.io/clusteradm/pkg/helpers"
-
-	"k8s.io/cli-runtime/pkg/printers"
+	workclient "open-cluster-management.io/api/client/work/clientset/versioned"
+	v1 "open-cluster-management.io/api/work/v1"
 )
 
 func (o *Options) complete(cmd *cobra.Command, args []string) (err error) {
@@ -41,13 +40,11 @@ func (o *Options) run() (err error) {
 	if err != nil {
 		return err
 	}
-
 	addonClient, err := addonclient.NewForConfig(restConfig)
 	if err != nil {
 		return err
 	}
-
-	kubeClient, apiExtensionsClient, dynamicClient, err := helpers.GetClients(o.ClusteradmFlags.KubectlFactory)
+	workClient, err := workclient.NewForConfig(restConfig)
 	if err != nil {
 		return err
 	}
@@ -70,57 +67,105 @@ func (o *Options) run() (err error) {
 
 	klog.V(3).InfoS("values:", "clusters", clusters)
 
-	return o.runWithClient(clusterClient, addonClient, kubeClient, apiExtensionsClient, dynamicClient, o.ClusteradmFlags.DryRun, clusters.List())
+	return o.printAddonTree(clusters.List(), addonClient, workClient)
 }
 
-func (o *Options) runWithClient(clusterClient clusterclientset.Interface,
+func (o *Options) printAddonTree(
+	clusters []string,
 	addonClient addonclient.Interface,
-	kubeClient kubernetes.Interface,
-	apiExtensionsClient apiextensionsclient.Interface,
-	dynamicClient dynamic.Interface,
-	dryRun bool,
-	clusters []string) error {
-
-	for _, clusterName := range clusters {
-		_, err := clusterClient.ClusterV1().ManagedClusters().Get(context.TODO(),
-			clusterName,
-			metav1.GetOptions{})
-		if err != nil {
-			return err
-		}
+	workClient workclient.Interface) error {
+	addonList, err := addonClient.AddonV1alpha1().
+		ManagedClusterAddOns(metav1.NamespaceAll).
+		List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+	addonByCluster := make(map[string][]*v1alpha1.ManagedClusterAddOn)
+	for _, addon := range addonList.Items {
+		clusterName := addon.Namespace
+		addon := addon
+		addonByCluster[clusterName] = append(addonByCluster[clusterName], &addon)
 	}
 
-	var addonlist v1alpha1.ManagedClusterAddOnList
-	for _, clusterName := range clusters {
-
-		list, err := addonClient.AddonV1alpha1().ManagedClusterAddOns(clusterName).List(context.TODO(),
-			metav1.ListOptions{})
-		if err != nil {
-			return err
-		}
-
-		addonlist.Items = append(addonlist.Items, list.Items...)
+	workList, err := workClient.WorkV1().
+		ManifestWorks(metav1.NamespaceAll).
+		List(context.TODO(), metav1.ListOptions{
+			LabelSelector: "open-cluster-management.io/addon-name",
+		})
+	if err != nil {
+		return err
 	}
 
-	printer := printers.NewTablePrinter(printers.PrintOptions{
-		NoHeaders:     false,
-		WithNamespace: true,
-		WithKind:      false,
-		Wide:          false,
-		ShowLabels:    false,
-		Kind: schema.GroupKind{
-			Group: "addon.open-cluster-management.io",
-			Kind:  "ManagedClusterAddOn",
-		},
-		ColumnLabels:     []string{},
-		SortBy:           "",
-		AllowMissingKeys: true,
-	})
-
-	sort.Slice(addonlist.Items, func(i, j int) bool {
-		return addonlist.Items[i].Name < addonlist.Items[j].Name
-	})
-
-	printer.PrintObj(&addonlist, o.Streams.Out)
+	root := gotree.New("<ManagedCluster>")
+	for _, clusterName := range clusters {
+		addonRoot := root.Add(color.New(color.FgBlue).Sprintf(clusterName))
+		addons, ok := addonByCluster[clusterName]
+		if !ok {
+			continue
+		}
+		for _, addon := range addons {
+			for _, work := range workList.Items {
+				if clusterName == work.Namespace && work.Labels["open-cluster-management.io/addon-name"] == addon.Name {
+					addonNode := addonRoot.Add(color.New(color.Bold).Sprintf("%s", addon.Name))
+					statusNode := addonNode.Add("<Status>")
+					printAddonStatus(statusNode, addon)
+					workNode := addonNode.Add(fmt.Sprintf("%s", "<ManifestWork>"))
+					printWorkDetail(workNode, &work)
+				}
+			}
+		}
+	}
+	fmt.Println(root.Print())
 	return nil
+}
+
+func printAddonStatus(n gotree.Tree, addon *v1alpha1.ManagedClusterAddOn) {
+	testingConds := []string{
+		"Available",
+		"ManifestApplied",
+		"RegistrationApplied",
+	}
+	sanitize := func(cond *metav1.Condition) string {
+		if cond == nil {
+			return "unknown"
+		}
+		if cond.Status == metav1.ConditionTrue {
+			return "true"
+		}
+		return color.RedString("false")
+	}
+	for _, condType := range testingConds {
+		cond := meta.FindStatusCondition(addon.Status.Conditions, condType)
+		n.Add(fmt.Sprintf("%s -> %s", condType, sanitize(cond)))
+	}
+}
+
+func printWorkDetail(n gotree.Tree, work *v1.ManifestWork) {
+	condByRs := make(map[string][]v1.ManifestCondition)
+	for _, cond := range work.Status.ResourceStatus.Manifests {
+		cond := cond
+		groupResource := schema.GroupResource{Group: cond.ResourceMeta.Group, Resource: cond.ResourceMeta.Resource}.String()
+		condByRs[groupResource] = append(condByRs[groupResource], cond)
+	}
+	for gr, rss := range condByRs {
+		rsNode := n.Add(gr)
+		for _, rs := range rss {
+			identifier := rs.ResourceMeta.Name
+			if len(rs.ResourceMeta.Namespace) > 0 {
+				identifier = rs.ResourceMeta.Namespace + "/" + identifier
+			}
+			rsNode.Add(fmt.Sprintf("%s (%s)", identifier, getManifestResourceStatus(&rs)))
+		}
+	}
+}
+
+func getManifestResourceStatus(manifestCond *v1.ManifestCondition) string {
+	appliedCond := meta.FindStatusCondition(manifestCond.Conditions, v1.WorkApplied)
+	if appliedCond == nil {
+		return "unknown"
+	}
+	if appliedCond.Status == metav1.ConditionTrue {
+		return "applied"
+	}
+	return color.RedString("not-applied")
 }

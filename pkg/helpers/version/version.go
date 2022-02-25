7f0695f75c6db8d3356a3f4308cb86e98a42748b
Min Kim <291271447@qq.com>
2022-02-25 14:58:47 +0800

    chore: defaulting latest release version and v-prefix compatible (#137)
    Signed-off-by: yue9944882 <291271447@qq.com>

--- pkg/helpers/version/version.go
+++ pkg/helpers/version/version.go
@@ -2,50 +2,55 @@
 package version
 
 import (
-	"errors"
+	"fmt"
+	"strings"
 )
 
-type VersionBundle struct {	
+type VersionBundle struct {
 	Registration string
-	Placement string
-	Work string
-	Operator string
-}	
+	Placement    string
+	Work         string
+	Operator     string
+}
 
+func GetVersionBundle(version string) (VersionBundle, error) {
 
-func GetVersionBundle(version string ) (VersionBundle ,error){
+	// supporting either "x.y.z" or "vx.y.z" format version
+	if strings.HasPrefix(version, "v") {
+		version = strings.TrimPrefix(version, "v")
+	}
 
 	versionBundleList := map[string]VersionBundle{}
 
-	// default 
-	versionBundleList["default"] = VersionBundle{
+	// latest
+	versionBundleList["latest"] = VersionBundle{
 		Registration: "latest",
-		Placement: "latest",
-		Work: "latest",
-		Operator: "latest",
+		Placement:    "latest",
+		Work:         "latest",
+		Operator:     "latest",
 	}
-	
-	// predifined bundle version 
-	versionBundleList["0.5.0"] = VersionBundle{
-		Registration: "0.5.0",
-		Placement: "0.2.0",
-		Work: "0.5.0",
-		Operator: "0.5.0",
 
+	// predifined bundle version
+	// TODO: automated version tracking
+	versionBundleList["0.5.0"] = VersionBundle{
+		Registration: "v0.5.0",
+		Placement:    "v0.2.0",
+		Work:         "v0.5.0",
+		Operator:     "v0.5.0",
 	}
 
 	versionBundleList["0.6.0"] = VersionBundle{
-		Registration: "0.6.0",
-		Placement: "0.3.0",
-		Work: "0.6.0",
-		Operator: "0.6.0",
+		Registration: "v0.6.0",
+		Placement:    "v0.3.0",
+		Work:         "v0.6.0",
+		Operator:     "v0.6.0",
 	}
 
+	// default
+	versionBundleList["default"] = versionBundleList["0.6.0"]
 
 	if val, ok := versionBundleList[version]; ok {
-		return val , nil
-	} 
-	no_version_error := errors.New("couldn't find the requested version bundle")
-	return  VersionBundle{}, no_version_error
+		return val, nil
+	}
+	return VersionBundle{}, fmt.Errorf("couldn't find the requested version bundle: %v", version)
 }
-

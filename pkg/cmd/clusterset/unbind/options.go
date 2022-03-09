8048b1645753665a4b6927f6c2a6d97aa0030d71
Yuchen YAO <37959723+ycyaoxdu@users.noreply.github.com>
2022-03-09 16:44:04 +0800

    feat: add command clusterset unbind to help unbind clusterset from namespace (#148)
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

---
+++ pkg/cmd/clusterset/unbind/options.go
@@ -0,0 +1,25 @@
+// Copyright Contributors to the Open Cluster Management project
+package unbind
+
+import (
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	genericclioptionsclusteradm "open-cluster-management.io/clusteradm/pkg/genericclioptions"
+)
+
+type Options struct {
+	//ClusteradmFlags: The generic optiosn from the clusteradm cli-runtime.
+	ClusteradmFlags *genericclioptionsclusteradm.ClusteradmFlags
+
+	Streams genericclioptions.IOStreams
+
+	Clusterset string
+
+	Namespace string
+}
+
+func newOptions(clusteradmFlags *genericclioptionsclusteradm.ClusteradmFlags, streams genericclioptions.IOStreams) *Options {
+	return &Options{
+		ClusteradmFlags: clusteradmFlags,
+		Streams:         streams,
+	}
+}

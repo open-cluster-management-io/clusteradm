4f45461429a85d1b9b50b909048998f244c51c51
Yuchen YAO <37959723+ycyaoxdu@users.noreply.github.com>
2022-10-20 17:29:01 +0800

    bug fix: apply cr after crd applied (#282)
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>
    
    Signed-off-by: ycyaoxdu <yaoyuchen0626@163.com>

--- pkg/cmd/join/exec.go
+++ pkg/cmd/join/exec.go
@@ -144,7 +144,7 @@ func (o *Options) run() error {
 	}
 	output = append(output, out...)
 
-	if o.wait && !o.ClusteradmFlags.DryRun {
+	if !o.ClusteradmFlags.DryRun {
 		if err := wait.WaitUntilCRDReady(apiExtensionsClient, "klusterlets.operator.open-cluster-management.io"); err != nil {
 			return err
 		}

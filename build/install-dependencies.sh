255a4d51bc45c13cef6d0e370b1e7a23ace8399c
zhujian <jiazhu@redhat.com>
2024-08-07 16:55:39 +0800

    Fix ci
    Signed-off-by: zhujian <jiazhu@redhat.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -6,8 +6,14 @@
 _OS=$(go env GOOS)
 _ARCH=$(go env GOARCH)
 
-if ! which patter > /dev/null;     then echo "Installing patter ..."; pushd $(mktemp -d) && GO111MODULE=off go get -u github.com/apg/patter && popd; fi
-if ! which gocovmerge > /dev/null; then echo "Installing gocovmerge..."; pushd $(mktemp -d) && GO111MODULE=off go get -u github.com/wadey/gocovmerge && popd; fi
+if ! which patter >/dev/null; then
+    echo "Installing patter ..."
+    go install github.com/apg/patter@latest
+fi
+if ! which gocovmerge >/dev/null; then
+    echo "Installing gocovmerge..."
+    go install github.com/wadey/gocovmerge@latest
+fi
 
 # Build tools
 

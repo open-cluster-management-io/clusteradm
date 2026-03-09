7b22e5a3344157d2b2aee3502941be4052c2a2d1
Tyler Gillson <tyler.gillson@gmail.com>
2026-03-09 16:33:14 -0600

    fix: report clusteradm version consistently; remove irrelevant server version
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -3,8 +3,121 @@ package version
 
 import (
 	"reflect"
+	"runtime/debug"
 	"testing"
 )
+
+func TestVersionFromBuildInfo(t *testing.T) {
+	tests := []struct {
+		name           string
+		info           *debug.BuildInfo
+		fallbackCommit string
+		wantVersion    string
+		wantCommit     string
+	}{
+		{
+			name: "tagged release via go install",
+			info: &debug.BuildInfo{
+				Main: debug.Module{Version: "v1.2.0"},
+				Settings: []debug.BuildSetting{
+					{Key: "vcs.revision", Value: "abc1234def5678"},
+					{Key: "vcs.modified", Value: "false"},
+				},
+			},
+			wantVersion: "v1.2.0",
+			wantCommit:  "abc1234",
+		},
+		{
+			name: "tagged release with dirty tree",
+			info: &debug.BuildInfo{
+				Main: debug.Module{Version: "v1.2.0"},
+				Settings: []debug.BuildSetting{
+					{Key: "vcs.revision", Value: "abc1234def5678"},
+					{Key: "vcs.modified", Value: "true"},
+				},
+			},
+			wantVersion: "v1.2.0-dirty",
+			wantCommit:  "abc1234",
+		},
+		{
+			name: "(devel) version is ignored",
+			info: &debug.BuildInfo{
+				Main: debug.Module{Version: "(devel)"},
+				Settings: []debug.BuildSetting{
+					{Key: "vcs.revision", Value: "abc1234def5678"},
+					{Key: "vcs.modified", Value: "false"},
+				},
+			},
+			wantVersion: "",
+			wantCommit:  "abc1234",
+		},
+		{
+			name: "(devel) with dirty tree does not append -dirty",
+			info: &debug.BuildInfo{
+				Main: debug.Module{Version: "(devel)"},
+				Settings: []debug.BuildSetting{
+					{Key: "vcs.revision", Value: "abc1234def5678"},
+					{Key: "vcs.modified", Value: "true"},
+				},
+			},
+			wantVersion: "",
+			wantCommit:  "abc1234",
+		},
+		{
+			name: "empty version is ignored",
+			info: &debug.BuildInfo{
+				Main:     debug.Module{Version: ""},
+				Settings: []debug.BuildSetting{},
+			},
+			wantVersion: "",
+			wantCommit:  "",
+		},
+		{
+			name: "short vcs.revision is ignored",
+			info: &debug.BuildInfo{
+				Main: debug.Module{Version: "v1.0.0"},
+				Settings: []debug.BuildSetting{
+					{Key: "vcs.revision", Value: "abc"},
+				},
+			},
+			wantVersion: "v1.0.0",
+			wantCommit:  "",
+		},
+		{
+			name:           "fallback commit is preserved when vcs.revision present",
+			fallbackCommit: "existing",
+			info: &debug.BuildInfo{
+				Main: debug.Module{Version: "v1.0.0"},
+				Settings: []debug.BuildSetting{
+					{Key: "vcs.revision", Value: "abc1234def5678"},
+				},
+			},
+			wantVersion: "v1.0.0",
+			wantCommit:  "existing",
+		},
+		{
+			name: "no vcs settings",
+			info: &debug.BuildInfo{
+				Main:     debug.Module{Version: "v0.10.1"},
+				Settings: []debug.BuildSetting{},
+			},
+			wantVersion: "v0.10.1",
+			wantCommit:  "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotVersion, gotCommit := versionFromBuildInfo(tt.info, tt.fallbackCommit)
+			if gotVersion != tt.wantVersion {
+				t.Errorf("version = %q, want %q", gotVersion, tt.wantVersion)
+			}
+			if gotCommit != tt.wantCommit {
+				t.Errorf("commit = %q, want %q", gotCommit, tt.wantCommit)
+			}
+		})
+	}
+}
 
 func TestGetVersionBundle(t *testing.T) {
 	expectedVersionBundle := VersionBundle{
